8fbf81992ad2b7619e52fc7e6fd4b64f3a873f3e
wayne-xiu <wenwuxiu1106@gmail.com>
2021-01-12 07:58:45 -0500

    Added Pilot project for Qt UI project

---
+++ QtWidgetPilot/src/Source.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QWidget window;
+    window.show();
+    return app.exec();
+}
