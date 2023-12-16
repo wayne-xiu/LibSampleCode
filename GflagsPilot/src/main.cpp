006c4ae9e43a6cc27cb5e4608d525b873201b037
Wenwu Xiu <wenwuxiu1106@gmail.com>
2023-12-16 08:27:48 -0500

    updated GflagsPilot with parent directory CMakeLists.txt and .clang-format

--- GflagsPilot/src/main.cpp
+++ GflagsPilot/src/main.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
 #include <gflags/gflags.h>
+
+#include <iostream>
 
 DEFINE_string(host, "localhost", "Server host address");
 DEFINE_int32(port, 8080, "Server port");
 
 // possible to have arguments validation
-static bool validatePort(const char *flag, google::int32 value)
-{
+static bool validatePort(const char* flag, google::int32 value) {
     if (value > 0 && value < 32768)
         return true;
     std::cerr << "Invalid value for --" << flag << ": " << value << std::endl;
     return false;
 }
-static const bool port_dummy = google::RegisterFlagValidator(&FLAGS_port, &validatePort);
+static const bool port_dummy =
+    google::RegisterFlagValidator(&FLAGS_port, &validatePort);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     google::SetVersionString("1.1.0");
     google::SetUsageMessage("./GflagsPilot");
     google::CommandLineFlagInfo info;
@@ -23,19 +23,17 @@ int main(int argc, char **argv)
     google::ParseCommandLineFlags(&argc, &argv, true);
 
     // check if parameters set
-    if (google::GetCommandLineFlagInfo("port", &info) && info.is_default)
-    {
+    if (google::GetCommandLineFlagInfo("port", &info) && info.is_default) {
         std::cout << "port is not set" << std::endl;
-    }
-    else
-    {
+    } else {
         std::cout << "port is set: " << FLAGS_port << std::endl;
     }
-    
+
     // dynamic setting
     std::cout << google::SetCommandLineOption("port", "9909") << std::endl;
     // introduction from env setting
-    std::cout << "Got '" << FLAGS_host << ":" << FLAGS_port << "'." << std::endl;
+    std::cout << "Got '" << FLAGS_host << ":" << FLAGS_port << "'."
+              << std::endl;
 
     return EXIT_SUCCESS;
 }
